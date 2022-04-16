b6fef4fa8397ffeb63e7647308ecf74c232d2f70
Daniel Gallups <dgallups@purdue.edu>
2022-04-16 13:39:22 -0400

    commit prior to potentially breaking sec fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from './NavBar';
 import Footer from './Footer';
 import Face from '../resources/face.jpg';
+import ReactComment from './ReactComment';
 import './Home.css';
 const Home = () => {
     return (
@@ -18,6 +19,7 @@ const Home = () => {
                             <div class="paragraph">I am a rising senior in Cybersecurity at Purdue University.</div>
                             <div class="paragraph">Here you can find all my information, including resume, CTF writeups, CS projects, and other points of interest!</div> 
                             <div class="paragraph">Perhaps you might even find my secret webpage...</div>
+                            <ReactComment text="Nice try, but it's not here :("/>
                             <div class="update">Last updated: April 16th, 2022</div>
                         </div>
                 </div>
