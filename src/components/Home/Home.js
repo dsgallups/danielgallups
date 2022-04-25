7781743bf97f03ca3c809c6beca57fefae098a44
Daniel Gallups <dgallups@purdue.edu>
2022-04-25 11:50:37 -0400

    css consolidation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -86,13 +86,13 @@ const Home = () => {
                 </ParallaxLayer>
             </div>
                 <ParallaxLayer sticky={{start:1, end: 1.2}} style={{...alignStart, justifyContent: 'flex-start'}}>\
-                    <div className="textbox skills-header">
+                    <div className="textbox skills-header large-header">
                         Interests
                     </div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={1.2} speed={.4} factor={1} style={{...alignStart, justifyContent: 'flex-start'}}>
                     <div className="textbox skill-container sdev">
-                        <div className="skill-header">
+                        <div className="skill-header small-header">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"
                             style={{
                                 height: "3em",
@@ -100,14 +100,14 @@ const Home = () => {
                             }}><path d="M414.8 40.79L286.8 488.8C281.9 505.8 264.2 515.6 247.2 510.8C230.2 505.9 220.4 488.2 225.2 471.2L353.2 23.21C358.1 6.216 375.8-3.624 392.8 1.232C409.8 6.087 419.6 23.8 414.8 40.79H414.8zM518.6 121.4L630.6 233.4C643.1 245.9 643.1 266.1 630.6 278.6L518.6 390.6C506.1 403.1 485.9 403.1 473.4 390.6C460.9 378.1 460.9 357.9 473.4 345.4L562.7 256L473.4 166.6C460.9 154.1 460.9 133.9 473.4 121.4C485.9 108.9 506.1 108.9 518.6 121.4V121.4zM166.6 166.6L77.25 256L166.6 345.4C179.1 357.9 179.1 378.1 166.6 390.6C154.1 403.1 133.9 403.1 121.4 390.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4L121.4 121.4C133.9 108.9 154.1 108.9 166.6 121.4C179.1 133.9 179.1 154.1 166.6 166.6V166.6z"/></svg>
                             <span>Software Development</span>
                         </div>
-                        <div className="skill-body">
+                        <div className="small-text">
                             Building products that improve people's lives is an intrinsic motivator. 
                         </div>
                     </div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={1.2} speed={.8} factor={1} style={{...alignStart, justifyContent: 'flex-start'}}>
                     <div className="textbox skill-container math">
-                        <div className="skill-header">
+                        <div className="skill-header small-header">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"
                             style={{
                                 height: "3em",
@@ -115,14 +115,14 @@ const Home = () => {
                             }}><path d="M414.8 40.79L286.8 488.8C281.9 505.8 264.2 515.6 247.2 510.8C230.2 505.9 220.4 488.2 225.2 471.2L353.2 23.21C358.1 6.216 375.8-3.624 392.8 1.232C409.8 6.087 419.6 23.8 414.8 40.79H414.8zM518.6 121.4L630.6 233.4C643.1 245.9 643.1 266.1 630.6 278.6L518.6 390.6C506.1 403.1 485.9 403.1 473.4 390.6C460.9 378.1 460.9 357.9 473.4 345.4L562.7 256L473.4 166.6C460.9 154.1 460.9 133.9 473.4 121.4C485.9 108.9 506.1 108.9 518.6 121.4V121.4zM166.6 166.6L77.25 256L166.6 345.4C179.1 357.9 179.1 378.1 166.6 390.6C154.1 403.1 133.9 403.1 121.4 390.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4L121.4 121.4C133.9 108.9 154.1 108.9 166.6 121.4C179.1 133.9 179.1 154.1 166.6 166.6V166.6z"/></svg>
                             <span>Mathematics</span>
                         </div>
-                        <div className="skill-body">
-                            Mathematics is a powerful tool for solving problems, and I have a particular interest in the field of Cryptography.
+                        <div className="small-text">
+                            As the language to define reality, most informed decisions require math. I have a particular interest in the field of Cryptography.
                         </div>
                     </div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={1.2} speed={1.6} factor={1} style={{...alignStart, justifyContent: 'flex-end'}}>
                     <div className="textbox skill-container csec">
-                        <div className="skill-header">
+                        <div className="skill-header small-header">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"
                             style={{
                                 height: "3em",
@@ -130,14 +130,14 @@ const Home = () => {
                             }}><path d="M414.8 40.79L286.8 488.8C281.9 505.8 264.2 515.6 247.2 510.8C230.2 505.9 220.4 488.2 225.2 471.2L353.2 23.21C358.1 6.216 375.8-3.624 392.8 1.232C409.8 6.087 419.6 23.8 414.8 40.79H414.8zM518.6 121.4L630.6 233.4C643.1 245.9 643.1 266.1 630.6 278.6L518.6 390.6C506.1 403.1 485.9 403.1 473.4 390.6C460.9 378.1 460.9 357.9 473.4 345.4L562.7 256L473.4 166.6C460.9 154.1 460.9 133.9 473.4 121.4C485.9 108.9 506.1 108.9 518.6 121.4V121.4zM166.6 166.6L77.25 256L166.6 345.4C179.1 357.9 179.1 378.1 166.6 390.6C154.1 403.1 133.9 403.1 121.4 390.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4L121.4 121.4C133.9 108.9 154.1 108.9 166.6 121.4C179.1 133.9 179.1 154.1 166.6 166.6V166.6z"/></svg>
                             <span>Cybersecurity</span>
                         </div>
-                        <div className="skill-body">
+                        <div className="small-text">
                             As a part of Purdue's CTF club, I have gained modest red-team experience. You will find my future writeups on a separate page.
                         </div>
                     </div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={1.2} speed={1.2} factor={1} style={{...alignStart, justifyContent: 'flex-end'}}>
                     <div className="textbox skill-container dvis">
-                        <div className="skill-header">
+                        <div className="skill-header small-header">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"
                             style={{
                                 height: "3em",
@@ -145,7 +145,7 @@ const Home = () => {
                             }}><path d="M414.8 40.79L286.8 488.8C281.9 505.8 264.2 515.6 247.2 510.8C230.2 505.9 220.4 488.2 225.2 471.2L353.2 23.21C358.1 6.216 375.8-3.624 392.8 1.232C409.8 6.087 419.6 23.8 414.8 40.79H414.8zM518.6 121.4L630.6 233.4C643.1 245.9 643.1 266.1 630.6 278.6L518.6 390.6C506.1 403.1 485.9 403.1 473.4 390.6C460.9 378.1 460.9 357.9 473.4 345.4L562.7 256L473.4 166.6C460.9 154.1 460.9 133.9 473.4 121.4C485.9 108.9 506.1 108.9 518.6 121.4V121.4zM166.6 166.6L77.25 256L166.6 345.4C179.1 357.9 179.1 378.1 166.6 390.6C154.1 403.1 133.9 403.1 121.4 390.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4L121.4 121.4C133.9 108.9 154.1 108.9 166.6 121.4C179.1 133.9 179.1 154.1 166.6 166.6V166.6z"/></svg>
                             <span>Data Visualization</span>
                         </div>
-                        <div className="skill-body">
+                        <div className="small-text">
                         A fascinating visualization is one that can articulate data in a meaningful way. I work on some projects using D3.js.
                         </div>
                     </div>
@@ -166,9 +166,9 @@ const Home = () => {
                             <div className="project-container">
                                 <div className="project-header">
                                     <img src={PathfinderIcon} alt="Pathfinder Logo" style={{width: '80px', height: '80px', marginBottom: '-10px'}}/>
-                                    <div className="project-title">Pathfinder</div>
+                                    <div className="small-header">Pathfinder</div>
                                 </div>
-                                <div className="project-body">
+                                <div className="project-body small-text">
                                     <span>No longer are the days of inaccurate college scheduling! Pathfinder enables students
                                     to optimize their plans of study. Features include quicker paths to graduation,
                                     search for certificates based on fewest credits, and much more.</span>
@@ -179,9 +179,9 @@ const Home = () => {
                             <div className="project-container">
                                 <div className="project-header">
                                     <img src={PurdueIcon} style={{width: '80px', height: '80px', marginBottom: '-10px'}}/>
-                                    <div className="project-title">iamboredatpurdue</div>
+                                    <div className="small-header">iamboredatpurdue</div>
                                 </div>
-                                <div className="project-body">
+                                <div className="project-body small-text">
                                     <span>Where is the bell tower? Why don't I have any homework? Why would I ever leave this place?
                                         If you're asking these questions, you're probably not a student at Purdue. But if you are,
                                         join us at iamboredatpurdue to find comfort in your boredom!
@@ -193,9 +193,9 @@ const Home = () => {
                             <div className="project-container">
                                 <div className="project-header">
                                     <img src={ChessBoardIcon} style={{width: '80px', height: '80px', marginTop: '5px', marginBottom: '0px'}}/>
-                                    <div className="project-title">Boilerchess</div>
+                                    <div className="small-header">Boilerchess</div>
                                 </div>
-                                <div className="project-body">
+                                <div className="project-body small-text">
                                     <span>If you're like me, then you would think that chess absolutely deserves a new piece.
                                         Behold! Boilerchess is a 9x9 tiled game that incorporates a new piece: The Boiler.
                                     </span>
