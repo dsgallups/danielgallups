423a787f516b7bbcea6abb1433c8543142be8571
dgallups-adversarial <daniel@adversarial.com>
2022-12-30 12:31:40 -0500

    vanilla rewrite

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export * from './NavBar';
-export * from './Footer';
-export * from './Aesthetics';
-export * from './UseBreakpoints';
-export * from './Breakpoint';
-/*WIP, need to refactor Home.js soon*/
