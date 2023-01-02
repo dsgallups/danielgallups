f47e2939f8ce28a769c3aa4a282f7ae4b71db58e
dgallups-adversarial <daniel@adversarial.com>
2023-01-02 00:13:15 -0500

    updates to media

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ const app = express();
 const path = require('path');
 
 app.get('/', (req, res) => {
+    console.log("GET on /static/");
     res.sendFile(path.join(__dirname +  '/content/index.html'))
 });
 
-app.get('/static/:path', (req, res) => {
-    res.sendFile(__dirname + "/content/static/" + req.params.path);
+app.get('/static/:type/:fileName', (req, res) => {
+    console.log("GET on /static/");
 
+    switch (req.params.type) {
+        case "media":
+            res.sendFile(__dirname + "/content/static/media/" + req.params.fileName);
+            break;
+        case "css":
+            res.sendFile(__dirname + "/content/static/css/" + req.params.fileName);
+            break;
+        default:
+            res.sendStatus(404);
+    }
 });
 
 const port = process.env.PORT || 3000;
