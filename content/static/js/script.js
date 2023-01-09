fa443ee52c37ccbf32d8cbae3fd8202134044a61
dsgallups <dgallups@purdue.edu>
2023-01-09 10:02:57 -0500

    added new mandelbrots, added stuff to script

---
+++ content/static/js/script.js
@@ -0,0 +1,33 @@
+/**
+ * THIS SCRIPT RENDERS THE PAGE.
+ */
+
+const skills = [
+    'Problem Solver',
+    'Problem Creator',
+    'Cybersecurity Analyst',
+    'Data Analyst',
+    'DNN Researcher',
+    'Software Developer',
+    'Penetration Tester',
+    'CTF Competitor',
+    'Code Documenter',
+    'Forensic Analyst',
+    'Rockstar',
+    'IT Specialist',
+    'Printer Fixer',
+    'Intelligence Gatherer',
+    'Code Writer',
+    'Coffee Maker',
+]
+
+const colorsOnWhite = [
+    '#CC0',
+    '#0DD',
+    '#F0F'
+]
+
+const main = () => {
+    console.log("working");
+}
+document.addEventListener('load', main);
