3cf1d47cc2973fb5fe683e7ccb9841465df71fd9
Daniel <dgallups@purdue.edu>
2023-11-28 17:42:50 -0500

    golfed assets

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,28 +14,12 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<strong class="text-xl uppercase">Daniel Gallups</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<a
 					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
+					href="https://github.com/dsgallups"
 					target="_blank"
 					rel="noreferrer"
 				>
