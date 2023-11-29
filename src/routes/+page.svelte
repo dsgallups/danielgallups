6b85048cb97fae6afc3525b49c0655d3cbe43a30
Daniel <dgallups@purdue.edu>
2023-11-28 20:17:56 -0500

    some styling, want to try background work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container h-full mx-auto flex flex-col pt-4">
+	<section>
+		<h1 class="text-4xl font-bold">Projects</h1>
+
+		<ul>
+			<li>Rust</li>
+			<ul>
+				<li>
+					<a href="https://github.com/dsgallups/mandelbrot">Mandelbrot Set Generator</a>
+					<div>A utility to generate the mandelbrot set with given parameters</div>
+				</li>
+				<li>
+					<a href="https://github.com/dsgallups/Pathfinder-RW">Pathfinder</a>
+					<div>
+						A project designed to optimize college schedules based on graduation path constraints
+					</div>
+				</li>
+			</ul>
+			<li>Python</li>
+		</ul>
+	</section>
+</div>
